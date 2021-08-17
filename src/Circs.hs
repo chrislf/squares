d9ce992a0687039e7a16ea275758358174eeb1b5
chrislf <chrislf@freeshell.org>
2021-08-17 23:50:00 +0100

    trig

--- src/Circs.hs
+++ src/Circs.hs
@@ -14,15 +14,13 @@ type Count = Float
 type Radius = Float
 type Time = Float
 
-xyToColour :: Float -> Float -> Float -> Float -> Color
-xyToColour x y xmax ymax = makeColor (sin (x/xmax)) (sin (y/ymax)) (x+y/xmax+ymax) 1
-
-xyToAlpha x y xmax ymax t = makeColor 1 1 1 (sin ((x+t)/xmax))
+xyToAlpha x y xmax ymax t = makeColor 1 1 1 ((1 + sin (t+x))/2)
 
 manyCircles :: Count -> Radius -> Time -> Picture
-manyCircles i r t = Pictures (time : pics)
+manyCircles i r t = Pictures (time: sint : pics)
   where
     --colouredCirc x y = Color (xyToColour (x+t*10) (y+t*10) (i*2) (i*2)) $ circleSolid r
     colouredCirc x y = Color (xyToAlpha x y (i*2) (i*2) t) $ circleSolid r
     pics = [ Translate (x*r*2) (y*r*2) (colouredCirc x y) | x <- [-i..i], y <- [-i..i] ]
     time = Translate (-i*r*2) (i*r*2.1) . Scale 0.25 0.25 . Color white . Text . show $ t
+    sint = Translate (-i*r*2) (i*r*2.2) . Scale 0.25 0.25 . Color white . Text . show $ sin t
